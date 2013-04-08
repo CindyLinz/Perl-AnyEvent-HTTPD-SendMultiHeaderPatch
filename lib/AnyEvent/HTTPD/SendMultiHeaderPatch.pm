2d25920610d961d72d3eb4bb877ad78443dcc3b1
Cindy Wang (CindyLinz) <cindylinz@gmail.com>
2013-04-08 21:31:30 +0800

    doc

--- lib/AnyEvent/HTTPD/SendMultiHeaderPatch.pm
+++ lib/AnyEvent/HTTPD/SendMultiHeaderPatch.pm
@@ -4,9 +4,12 @@ use 5.006;
 use strict;
 use warnings FATAL => 'all';
 
+use AnyEvent::HTTPD::HTTPConnection;
+
 =head1 NAME
 
-AnyEvent::HTTPD::SendMultiHeaderPatch - The great new AnyEvent::HTTPD::SendMultiHeaderPatch!
+AnyEvent::HTTPD::SendMultiHeaderPatch -
+    Patch (hack) of AnyEvent::HTTPD for sending multiple headers with the same field name.
 
 =head1 VERSION
 
@@ -16,84 +19,186 @@ Version 0.01
 
 our $VERSION = '0.01';
 
+use AnyEvent::HTTPD;
+
+package AnyEvent::HTTPD::Util;
+
+use strict;
+use warnings;
+
+our @EXPORT;
+push @EXPORT, qw(header_add header_gets);
+
+sub header_add {
+    my ($hdrs, $name, $value) = @_;
+    $name = _header_transform_case_insens ($hdrs, $name);
+    if( exists $hdrs->{$name} ) {
+        $hdrs->{$name} .= "\0".$value;
+    }
+    else {
+       $hdrs->{$name} = $value;
+    }
+}
+
+sub header_gets {
+    my ($hdrs, $name) = @_;
+    $name = _header_transform_case_insens ($hdrs, $name);
+    exists $hdrs->{$name} ? [split /\0/, $hdrs->{$name}] : []
+}
+
+package AnyEvent::HTTPD::HTTPConnection;
+
+use strict;
+use warnings;
+no warnings 'redefine';
+
+sub response {
+   my ($self, $code, $msg, $hdr, $content, $no_body) = @_;
+   return if $self->{disconnected};
+   return unless $self->{hdl};
+
+   my $res = "HTTP/1.0 $code $msg\015\012";
+   header_set ($hdr, 'Date' => _time_to_http_date time)
+      unless header_exists ($hdr, 'Date');
+   header_set ($hdr, 'Expires' => header_get ($hdr, 'Date'))
+      unless header_exists ($hdr, 'Expires');
+   header_set ($hdr, 'Cache-Control' => "max-age=0")
+      unless header_exists ($hdr, 'Cache-Control');
+   header_set ($hdr, 'Connection' =>
+                    ($self->{keep_alive} ? 'Keep-Alive' : 'close'));
+
+   header_set ($hdr, 'Content-Length' => length "$content")
+      unless header_exists ($hdr, 'Content-Length')
+             || ref $content;
+
+   unless (defined header_get ($hdr, 'Content-Length')) {
+      # keep alive with no content length will NOT work.
+      delete $self->{keep_alive};
+      header_set ($hdr, 'Connection' => 'close');
+   }
+
+   while (my ($h, $v) = each %$hdr) {
+      next unless defined $v;
+      for my $vv ( split /\0/, $v ) {
+          $res .= "$h: $vv\015\012";
+      }
+   }
+
+   $res .= "\015\012";
+
+   if ($no_body) { # for HEAD requests!
+      $self->{hdl}->push_write ($res);
+      $self->response_done;
+      return;
+   }
+
+   if (ref ($content) eq 'CODE') {
+      weaken $self;
+
+      my $chunk_cb = sub {
+         my ($chunk) = @_;
+
+         return 0 unless defined ($self) && defined ($self->{hdl}) && !$self->{disconnected};
+
+         delete $self->{transport_polled};
+
+         if (defined ($chunk) && length ($chunk) > 0) {
+            $self->{hdl}->push_write ($chunk);
+
+         } else {
+            $self->response_done;
+         }
+
+         return 1;
+      };
+
+      $self->{transfer_cb} = $content;
+
+      $self->{hdl}->on_drain (sub {
+         return unless $self;
+
+         if (length $res) {
+            my $r = $res;
+            undef $res;
+            $chunk_cb->($r);
+
+         } elsif (not $self->{transport_polled}) {
+            $self->{transport_polled} = 1;
+            $self->{transfer_cb}->($chunk_cb) if $self;
+         }
+      });
+
+   } else {
+      $res .= $content;
+      $self->{hdl}->push_write ($res);
+      $self->response_done;
+   }
+}
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
+    use AnyEvent::HTTPD; # Optional,
+                         # because the patch module will use it first.
     use AnyEvent::HTTPD::SendMultiHeaderPatch;
 
-    my $foo = AnyEvent::HTTPD::SendMultiHeaderPatch->new();
-    ...
+    # In the http request handler,
+    # separate the multiple values of the same field with \0 character.
+    sub {
+        my($httpd, $req) = @_;
+        # ...
+        $req->respond(
+            200, 'OK', {
+                'Set-Cookie' => "a=123; path=/; domain=.example.com\0b=456; path=/; domain=.example.com"
+            }, "Set the cookies"
+        );
+    }
 
-=head1 EXPORT
+    # Or use the added util function header_add in AnyEvent::HTTPD::Util.
+    use AnyEvent::HTTPD::Util;
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    sub {
+        my($httpd, $req) = @_;
+        # ...
+        my %header;
+        header_add(\%header, 'Set-Cookie', 'a=123; path=/; domain=.example.com');
+        header_add(\%header, 'Set-Cookie', 'b=456; path=/; domain=.example.com');
+        $req->respond(200, 'OK', \%header, "Set the cookies");
+    }
 
-=head1 SUBROUTINES/METHODS
+    # There also introduced another util function header_gets in AnyEvent::HTTPD::Util,
+    # to extract multiple values in the header
+    sub {
+        my($httpd, $req) = @_;
+        # ...
+        my %header;
+        header_add(\%header, 'Example', 'a');
+        header_add(\%header, 'Example', 'b');
 
-=head2 function1
+        my $example_values = header_gets(\%header, 'Example');
+        # get ['a', 'b']
+        my $no_values = header_gets(\%header, 'None');
+        # get []
+    }
 
-=cut
 
-sub function1 {
-}
+=head1 CAVEATS
 
-=head2 function2
+=over 4
 
-=cut
+=item This is a hack (should be stable)
 
-sub function2 {
-}
+This module is a hack patch that replace the method 'response' in package AnyEvent::HTTPD::HTTPConnection.
+I think that it's still stable since the module AnyEvent::HTTPD has been frozen since 2011.3 (Today is 2013.4)
+
+=item No \0 in your header values
+
+Don't use \0 in your header values since it's used as the separater.
+
+=back
 
 =head1 AUTHOR
 
 Cindy Wang (CindyLinz)
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-anyevent-httpd-sendmultiheaderpatch at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=AnyEvent-HTTPD-SendMultiHeaderPatch>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc AnyEvent::HTTPD::SendMultiHeaderPatch
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=AnyEvent-HTTPD-SendMultiHeaderPatch>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/AnyEvent-HTTPD-SendMultiHeaderPatch>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/AnyEvent-HTTPD-SendMultiHeaderPatch>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/AnyEvent-HTTPD-SendMultiHeaderPatch/>
-
-=back
-
-
-=head1 ACKNOWLEDGEMENTS
-
 
 =head1 LICENSE AND COPYRIGHT
 
